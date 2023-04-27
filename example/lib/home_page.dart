947037d200d0d6463fdee1a247e192509d7327ca
Zach <50252234+ZM009@users.noreply.github.com>
2023-04-27 01:48:39 -0400

    added home page, and fixed widget test

---
+++ example/lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+/// will be uncommented when release is created
+//import 'package:control_pad_plus/control_pad_plus.dart
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Joystick Controller Example"),
+      ),
+      body: Container(
+        color: Colors.red,
+        //child: JoystickView(),
+      ),
+    );
+  }
+}
