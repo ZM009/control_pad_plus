e08efe21f884711eda5302be44ca95cf49ee5cae
Zach <50252234+ZM009@users.noreply.github.com>
2023-04-26 21:51:35 -0400

    exported proper views

--- lib/control_pad_plus.dart
+++ lib/control_pad_plus.dart
@@ -1,7 +1,4 @@
 library control_pad_plus;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'views/joystick_view.dart';
+export 'views/pad_button_view.dart';
