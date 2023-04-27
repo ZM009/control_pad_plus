860c810e0c88c389994c884744ab724c1aa4d7b6
Zach <50252234+ZM009@users.noreply.github.com>
2023-04-27 00:40:07 -0400

    added test

--- test/control_pad_plus_test.dart
+++ test/control_pad_plus_test.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:control_pad_plus/control_pad_plus.dart';
-
-void main() {}
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp());
+  });
+}
